f74246d1f436c19c90961c3b68abddaf9da023c6
Amara <amar_algu@hotmail.com>
2021-11-24 10:19:21 -0500

    server graphql working

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const {
   createUser,
   getSingleUser,
-  saveGoal,
-  deleteGoal,
+  // saveGoal,
+  // deleteGoal,
   login,
 } = require('../../controllers/user-controller');
 
@@ -11,10 +11,10 @@ const {
 const { authMiddleware } = require('../../utils/auth');
 
 // put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authMiddleware, saveGoal);
+router.route('/').post(createUser).put(authMiddleware,getSingleUser);
 
 router.route('/login').post(login);
-
-router.route('/goals/:goalId').delete(authMiddleware, deleteGoal);
+router.route('/me').get(authMiddleware, getSingleUser);
+// router.route('/goals/:goalId').delete(authMiddleware );
 
 module.exports = router;
