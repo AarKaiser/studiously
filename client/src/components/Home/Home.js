06add3cc09d18e3d65f3089ed04f6651911c642b
Aar Kaiser <dev@aarkaiser>
2021-11-24 01:37:45 -0500

    working version

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react"
 import './Home.css'
+import LoginSignup from "./LoginSignup/LoginSignup";
 
 export default function Home(props) {
     return (
         <div className='home-container'>
             <p>Home Works!</p>
+            <LoginSignup />
         </div>
     )}
--- client/src/components/Home/LoginSignup/LoginSignup.js
+++ client/src/components/Home/LoginSignup/LoginSignup.js
@@ -1,16 +1,19 @@
 import FacebookLogin from "./Facebook/FacebookLogin";
 import GoogleLogin from "./Google/GoogleLogin";
 import GoogleLogout from "./Google/GoogleLogout";
+import LoginSignup from "./Internal/InternalLoginSignup"
 
 export default function Home(props) {
     return (
         <div className='home-container'>
-            <p>Signin & Signup Test Environment</p>
+            <p>Signin/Signup Test Environment</p>
             <p>Facebook Login</p>
             <FacebookLogin />
             <p>Google Login</p>
             <GoogleLogin />
             <p>Google Logout</p>
-            <GoogleLoglout />
+            <GoogleLogout />
+            <p>Internal</p>
+            <LoginSignup />
         </div>
     )}
