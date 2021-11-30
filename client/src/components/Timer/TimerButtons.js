831cf5ba7859ff0f1290e5c19068cfdb767cfb14
navjot turna <shamsherturna101@gmail.com>
2021-11-29 20:12:40 -0500

    timer style

--- client/src/components/Timer/TimerButtons.js
+++ client/src/components/Timer/TimerButtons.js
@@ -10,14 +10,14 @@ export default function ReviewButtons({
   return (
     <div className="flex justify-center space-x-10 timer-style ">
       <button
-        className="bg-green-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded bottom-1 justify-start"
+        className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded bottom-1 justify-start"
         onClick={!paused ? pauseCountDown : startCountDown}
       >
         {!paused ? 'Pause' : 'Resume'}
       </button> 
       <button
         onClick={moveToNextGoal}
-        className="content-end bg-green-600 blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded -bottom-1 justify-end"
+        className="content-end bg-green-600 blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded -bottom-1 justify-end"
         disabled
       >
         Next Goal
