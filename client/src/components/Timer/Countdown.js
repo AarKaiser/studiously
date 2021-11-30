5c701efbf73ea9d4d1c7b542d7e1ec6eadfe8709
navjot turna <shamsherturna101@gmail.com>
2021-11-29 19:08:56 -0500

    updated timer

--- client/src/components/Timer/Countdown.js
+++ client/src/components/Timer/Countdown.js
@@ -4,7 +4,7 @@ const dummyTime = '1:20:3'.split(':').map((time) => +time);
 
 function Countdown({ hours, minutes, seconds }) {
   return (
-    <div className="flex justify-center space-x-10">
+    <div className="flex justify-center space-x-10 font-bold">
       {/* {timerComponents.length ? timerComponents : <span>Time's up!</span>} */}
       <h3>Hours: {hours}</h3>
       <h3>Minutes: {minutes}</h3>
